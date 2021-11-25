23808c7f0f183ba7dcbe6277a9d589533348e030
Max Phillips <max@maxphillipsdev.com>
2021-11-25 17:12:46 +1100

    yay

--- components/shell/NavBar.tsx
+++ components/shell/NavBar.tsx
@@ -1,14 +1,29 @@
-import { Heading } from "@chakra-ui/react";
+import { Heading, Avatar, Button } from "@chakra-ui/react";
+import { signIn, useSession } from "next-auth/react";
 import NavBarContainer from "./NavBarContainer";
 
 interface Props {}
 
 const NavBar: React.FC<Props> = (props) => {
+  const { data: session } = useSession();
+
+  const handleSignIn = () => signIn();
+
   return (
     <NavBarContainer {...props}>
-      <Heading as="h1" fontSize="2rem">
+      <Heading as="h1" fontSize="2rem" color="black">
         HeapUnderflow
       </Heading>
+      {session ? (
+        <Avatar
+          name={session.user?.name || ""}
+          src={session.user?.image || ""}
+        />
+      ) : (
+        <Button color="black" onClick={handleSignIn}>
+          Sign in
+        </Button>
+      )}
     </NavBarContainer>
   );
 };
